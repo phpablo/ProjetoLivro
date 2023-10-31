6fdbc3a84b27a5c79376a25fba87ddfa480672aa
phpablo <pablohenrique120@gmail.com>
2023-10-31 11:42:24 -0300

    :racehorse: Instancia os objetos teste -Testando como funciona o sistema de classes e objetos de suas instâncias

--- index.php
+++ index.php
@@ -8,11 +8,23 @@
 </head>
 
 <body>
-  <?php
+  <pre>
+    <?php
+    require_once 'Pessoa.php';
+    require_once 'Livro.php';
+
+    $p[0] = new Pessoa('Gohan', 11, 'masculino');
+    $p[1] = new Pessoa('Bulma', 15, 'feminino');
+
+    $l[0] = new Livro('Protocolo Bluehand', 'Jovem Nerd', 120, $p[0]);
+    $l[1] = new Livro('PHPBásico', 'José da Silva', 400, $p[0]);
+    $l[2] = new Livro('Hogwarts:Uma História', 'Batilda Bagshot', 600, $p[1]);
+
+    print_r($l[0]);
 
 
-
-  ?>
+    ?>
+  </pre>
 </body>
 
 </html>
